7ffee7e0722d6d8bd9760e7ab229b702dc861fb7
Tjardo Ihrig <tjardoihrig.zakelijk@gmail.com>
2023-09-19 10:06:04 +0200

    Cloning earlier project

---
+++ app/config/config.php
@@ -0,0 +1,9 @@
+<?php
+
+define("URLROOT", "http://be-opdracht2-j2-p1.com");
+define("APPROOT", dirname(dirname(__FILE__)));
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'instructeursindienst');
